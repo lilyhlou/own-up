9407e04d084b856acb980dd86029113fd15a506c
lilyhlou <lilylou16@gmail.com>
2020-12-09 23:00:13 -0500

    Fixed validate method in Input

--- src/components/Input.js
+++ src/components/Input.js
@@ -13,62 +13,67 @@ class InputForm extends Component {
 		this.state = { 
 		  search: {}, // form inputs are kept in state of input component
 		  submitted: false, // submitted value passed to table.js
-		  loanErr: false,
+		  // keeping track of form inputs for error message
+		  loanErr: false, 
 		  propertyErr: false,
 		  creditErr: false,
 		  occupancyErr: false,
 		  errString: "" 
 		};
 		this.submit = this.submit.bind(this);
-	  }
-	  validate() {
-		  let search = this.state.search;
-		  let valid = true;
-		  
-		  search["loan"] = parseInt(search["loan"]);  
-		  search["credit"] = parseInt(search["credit"]); // convert to string              
-		  this.setState({search});
-  
-		if(isNaN(search["loan"])) { // check if int (parseInt turns string into int if int, to NaN if unable to parse) and if loan exists 
-				valid = false;
-				this.setState({loanErr: true});
-				this.setState({errString: "Loan input must be a whole number and can not contain any punctuation."});
+	}
+	validate() {
+		let search = this.state.search;
+		let valid = true;
+		
+		// convert input to int from string
+		if(search["loan"]==="" || isNaN(parseInt(search["loan"]))) { // check if int (parseInt turns string into int if int, to NaN if unable to parse) and if loan exists 
+			valid = false;
+			this.setState({loanErr: true});
+			if(search["loan"]==="") {
+				this.setState({errString: "Insert whole numbers in loan size field."});
 			} else {
-				this.setState({loanErr: false});
-			}
+				this.setState({errString: "Loan input must be a whole number and can not contain any punctuation."});
+			} 
+		} else {
+			this.setState({loanErr: false});
+		}
 
-		  if(!search["property"] || search["property"] === "") {
-			  valid = false;
-			  this.setState({propertyErr: true});
-			  this.setState({errString: "Select value from property drop down."});
-		  } else {
+		if(!search["property"] || search["property"] === "") {
+			valid = false;
+			this.setState({propertyErr: true});
+			this.setState({errString: "Select property type value from drop down."});
+		} else {
 			this.setState({propertyErr: false});
-		  }
+		}
 
-		  if(isNaN(search["credit"])) { // either is nonexistent or 
+		if(isNaN(parseInt(search["credit"])) || search["credit"] < 300 || search["credit"] > 800 || search["credit"] === "") { // either is nonexistent or not numeric
 			valid = false;
 			this.setState({creditErr: true});
-			this.setState({errString: "Insert numerical value for credit field."});
-		} else if(search["credit"] < 300 || search["credit"] > 800 ) { // check if int and if credit is between 300 and 800
-			  valid = false;
-			  this.setState({creditErr: true});
-			  this.setState({errString: "Credit score must be a whole number between 300 and 800 and can not include any puncuation."});
-		  } else {
+		  	if(search["credit"] < 300 || search["credit"] > 800 ) { // check if credit is between 300 and 800
+				this.setState({errString: "Credit score must be a whole number between 300 and 800."});
+			} else if (search["credit"] === "") {
+				this.setState({errString: "Insert whole numbers in credit score field."});
+			} else {
+				this.setState({errString: "Credit score field can not include any puncuation or letters."});
+			}
+		} else {
 			this.setState({creditErr: false});
-		  }
+		}
 
-		  if(!search["occupancy"] || search["occupancy"] === "") {
+		if(!search["occupancy"] || search["occupancy"] === "") {
 			valid = false;
 			this.setState({occupancyErr: true});
-			this.setState({errString: "Select value from occupancy drop down."});
-		  } else {
+			this.setState({errString: "Select occupancy value from drop down."});
+		} else {
 			this.setState({occupancyErr: false});
-		  }
+		}
+
 		if(valid) { // return to normal if there was prev. an error
 			this.setState({errString: ""});
 		}
 		return valid;
-	  }
+	}
 	  submit(e) {
 		  e.preventDefault();
 		  if(this.validate()) { // ensure that data entered is within bounds
@@ -76,7 +81,7 @@ class InputForm extends Component {
 			this.props.fetchData(`https://ss6b2ke2ca.execute-api.us-east-1.amazonaws.com/Prod/quotes?loanSize=${this.state.search["loan"]}&creditScore=${this.state.search["credit"]}&propertyType=${this.state.search["property"]}&occupancy=${this.state.search["occupancy"]}`);
 		} 
 	  }  
-	  update = (e) => {
+	  update = (e) => { // when user types or changes selection in form 
 		let search = this.state.search;
 		search[e.target.name] = e.target.value;        
 		this.setState({search});
@@ -84,78 +89,74 @@ class InputForm extends Component {
 	render() {
 		return(
 			<Container fluid={true}>
-			<Form id="forMedScreen">
-  				<Form.Row>
-					<Form.Group as={Col} md className="d-flex justify-content-end">     
-						<div className="form-inline">
-						<Form.Label>Loan Size</Form.Label>
-						<InputGroup className={"formBox mb-3" + (this.state.loanErr ? ' error' : ' noerror')}>
-						<div className="flex"></div>
-						<InputGroup.Prepend>
-      						<InputGroup.Text>$</InputGroup.Text>
-    					</InputGroup.Prepend>
-						<FormControl type="text" placeholder="450,000" size="md" name="loan" onChange={(e) => this.update(e)}/>
-						</InputGroup>
-						</div>
-					</Form.Group>
-					<Form.Group as={Col} md className="d-flex justify-content-end"> 
-						<div className="form-inline">
-						<Form.Label>Property Type</Form.Label>
-						<Form.Control as="select" defaultValue={'DEFAULT'} size="md" name="property" className={"formBox" + (this.state.propertyErr ? ' error' : ' noerror')} onChange={(e) => this.update(e)}>
-							<option value="DEFAULT" disabled hidden>Select Option</option>
-							<option value="SingleFamily">Single Family</option>
-							<option value="Condo">Condo</option>
-							<option value="Townhouse">Townhouse</option>
-							<option value="MultiFamily">Multi-Family</option>
-						</Form.Control>
-						</div>
-					</Form.Group>
-  				</Form.Row>
-  				<Form.Row>
-    				<Form.Group as={Col} md className="d-flex justify-content-end">
-						<div className="form-inline">
-						<Form.Label>Credit Score</Form.Label>
-						<FormControl type="text" placeholder="300-800" id="bar" size="md" name="credit" className={"formBox" + (this.state.creditErr ? ' error' : ' noerror')} onChange={(e) => this.update(e)}/>
-						</div>
-					</Form.Group>
-    				<Form.Group as={Col} md className="d-flex justify-content-end">
-						<div className="form-inline">
-							<Form.Label>Occupancy</Form.Label>
-							<Form.Control as="select" size="md" name="occupancy" className={"formBox" + (this.state.occupancyErr ? ' error' : ' noerror')} defaultValue={'DEFAULT'} onChange={(e) => this.update(e)}>
-								<option value="DEFAULT" disabled hidden>Select Option</option>
-								<option value="Primary">Primary Residence</option>
-								<option value="Secondary">Secondary Residence</option>
-								<option value="Investment">Investment</option>
-							</Form.Control>
-						</div>
-					</Form.Group>
-  				</Form.Row>
-				<Form.Row>
-					<Form.Group as={Col} md className="d-flex justify-content-end button">
-						<Button
-							type="submit"
-							className="blackBackground"
-							onClick={this.submit}
-						>
-							{this.props.isLoading ? 'Loading…' : 'Quote Rates'}
-						</Button>
-					</Form.Group>
-				</Form.Row>
-  			</Form>
-			  <Row>
-			  <Col>
-			<Alert variant="danger" className={(this.state.errString !== "") ? 'd-block' : 'd-none'}>
-    			{this.state.errString}
-  			</Alert>
-			  </Col>
-
-			  </Row >
-			  <Row className="d-flex">
-			<RateTable
-				submitted={this.state.submitted}
-			>
-			</RateTable>
-			</Row>
+				<Form id="forMedScreen">
+					<Form.Row>
+						<Form.Group as={Col} md className="d-flex justify-content-end">     
+								<div className="form-inline">
+									<Form.Label>Loan Size</Form.Label>
+									<InputGroup className={"formBox mb-3" + (this.state.loanErr ? ' error' : ' noerror')}>
+										<InputGroup.Prepend>
+											<InputGroup.Text>$</InputGroup.Text>
+										</InputGroup.Prepend>
+										<FormControl type="text" placeholder="450,000" size="md" name="loan" onChange={(e) => this.update(e)}/>
+									</InputGroup>
+								</div>
+						</Form.Group>
+						<Form.Group as={Col} md className="d-flex justify-content-end"> 
+							<div className="form-inline">
+								<Form.Label>Property Type</Form.Label>
+								<Form.Control as="select" defaultValue={'DEFAULT'} size="md" name="property" className={"formBox" + (this.state.propertyErr ? ' error' : ' noerror')} onChange={(e) => this.update(e)}>
+									<option value="DEFAULT" disabled hidden>Select Option</option>
+									<option value="SingleFamily">Single Family</option>
+									<option value="Condo">Condo</option>
+									<option value="Townhouse">Townhouse</option>
+									<option value="MultiFamily">Multi-Family</option>
+								</Form.Control>
+							</div>
+						</Form.Group>
+					</Form.Row>
+					<Form.Row>
+						<Form.Group as={Col} md className="d-flex justify-content-end">
+							<div className="form-inline">
+								<Form.Label>Credit Score</Form.Label>
+								<FormControl type="text" placeholder="300-800" size="md" name="credit" className={"formBox" + (this.state.creditErr ? ' error' : ' noerror')} onChange={(e) => this.update(e)}/>
+							</div>
+						</Form.Group>
+						<Form.Group as={Col} md className="d-flex justify-content-end">
+							<div className="form-inline">
+								<Form.Label>Occupancy</Form.Label>
+								<Form.Control as="select" size="md" name="occupancy" className={"formBox" + (this.state.occupancyErr ? ' error' : ' noerror')} defaultValue={'DEFAULT'} onChange={(e) => this.update(e)}>
+									<option value="DEFAULT" disabled hidden>Select Option</option>
+									<option value="Primary">Primary Residence</option>
+									<option value="Secondary">Secondary Residence</option>
+									<option value="Investment">Investment</option>
+								</Form.Control>
+							</div>
+						</Form.Group>
+					</Form.Row>
+					<Form.Row>
+						<Form.Group as={Col} md className="d-flex justify-content-end button">
+							<Button
+								type="submit"
+								className="blackBackground"
+								onClick={this.submit}
+							>
+								{this.props.isLoading ? 'Loading…' : 'Quote Rates'}
+							</Button>
+						</Form.Group>
+					</Form.Row>
+				</Form>
+				<Row>
+					<Col>
+						<Alert variant="danger" className={(this.state.errString !== "") ? 'd-block' : 'd-none'}>
+							{this.state.errString}
+						</Alert>
+					</Col>
+				</Row >
+				<Row className="d-flex">
+					<RateTable submitted={this.state.submitted}>
+					</RateTable>
+				</Row>
 		</Container>
 		);
 	}
--- src/components/Table.js
+++ src/components/Table.js
@@ -5,49 +5,48 @@ import { Table, Alert, Row, Col } from 'react-bootstrap';
 
 class RateTable extends Component {
     render() {
-        if (!this.props.submitted || this.props.isLoading) {
+        if (!this.props.submitted || this.props.isLoading) { 
             return <p></p>;
         }
-        if(this.props.submitted && this.props.items.length === 0) {
-            return 	<Alert variant="warning">
+        if(this.props.submitted && this.props.items.length === 0) { // api returned no results in items list
+            return <Alert variant="warning">
                         No results found.  
                     </Alert>
-
         }
-        if (this.props.hasErrored) {
+        if (this.props.hasErrored) { // error from API 
             return 	<Row>
-            <Col>
-          <Alert variant="danger" className='d-block'>
-          Sorry, there was an error loading the items. Try refreshing and submitting again.
-            </Alert>
-            </Col>
-            </Row>
+                        <Col>
+                            <Alert variant="danger" className='d-block'>
+                                Sorry, there was an error loading the items. Try refreshing and submitting again.
+                            </Alert>
+                        </Col>
+                    </Row>
         }
         return (
             <Table responsive>
-  <thead>
-    <tr>
-        <th>LENDER</th>
-        <th>PRODUCT</th>
-        <th >RATE</th>
-        <th>CLOSING COSTS</th>
-        <th>MONTHLY PAYMENT</th>
-        <th>APR</th>
-    </tr>
-  </thead>
-  <tbody>
-      {this.props.items.map((item, i) => (
-                <tr key={i}>
-                   <td>{(item.lenderName.length > 14) ? item.lenderName.substr(0, 13) + '...' : item.lenderName}</td> 
-                   <td >{item.loanType}</td> 
-                   <td >{item.interestRate}%</td> 
-                   <td >${item.closingCosts.toFixed(2)}</td> 
-                   <td >${item.monthlyPayment.toFixed(2)}</td> 
-                   <td>{item.apr.toFixed(3)}%</td> 
-                </tr>
-                ))}
-  </tbody>
-</Table>
+                <thead>
+                    <tr>
+                        <th>LENDER</th>
+                        <th>PRODUCT</th>
+                        <th >RATE</th>
+                        <th>CLOSING COSTS</th>
+                        <th>MONTHLY PAYMENT</th>
+                        <th>APR</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.items.map((item, i) => (
+                        <tr key={i}>
+                        <td>{(item.lenderName.length > 14) ? item.lenderName.substr(0, 13) + '...' : item.lenderName}</td> 
+                        <td >{item.loanType}</td> 
+                        <td >{item.interestRate.toFixed(3)}%</td> 
+                        <td >${item.closingCosts.toFixed(2)}</td> 
+                        <td >${item.monthlyPayment.toFixed(2)}</td> 
+                        <td>{item.apr.toFixed(3)}%</td> 
+                        </tr>
+                        ))}
+                </tbody>
+            </Table>
         );
     }
 }
